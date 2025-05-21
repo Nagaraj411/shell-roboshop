08cb7b03de4293e88ba7ffe72594847aa21f88ed
Nagaraj <nagarajuande255@gmail.com>
2025-05-21 10:53:05 +0530

    catalogue.service

--- catalogue.sh
+++ catalogue.sh
@@ -84,3 +84,8 @@ VALIDATE $? "mongodb installation"
 
 mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
 VALIDATE $? "mongodb data import"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
