0c9c7d2c63ccdfb6542d5631ad2b70a10e3392dc
Nagaraj <nagarajuande255@gmail.com>
2025-05-21 08:51:02 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -2,19 +2,22 @@
 
 AMI_ID="ami-09c813fb71547fc4f" # replace with your AMI ID
 SG_ID="sg-009d0f9988cd2de9b" # replace with your Security groups ID
-INSTANCES=("mongodb" "redis" "frontend") #"mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch"
+INSTANCES=("mongodb" "redis" "frontend" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch")
 ZONE_ID="Z05005862BAG0R5BQ5WUP" # replace with your ZONE ID
 DOMAIN_NAME="devops84.shop" # replace with your domain
 
-for instance in ${INSTANCES[@]}     #for instance in $@
+#for instance in ${INSTANCES[@]}     
+for instance in $@
 
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-009d0f9988cd2de9b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
@@ -26,7 +29,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            "Name"              : "'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
