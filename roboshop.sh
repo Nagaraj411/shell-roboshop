d0be80172bfc1229c09999b742d80591b3ddd040
Nagaraj <nagarajuande255@gmail.com>
2025-05-21 06:20:41 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z05005862BAG0R5BQ5WUP" # replace with your ZONE ID
 DOMAIN_NAME="devops84.shop" # replace with your domain
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
