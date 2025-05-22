ce13f487399768a5797b693dd08886861c1c36e0
Nagaraj <nagarajuande255@gmail.com>
2025-05-22 10:34:15 +0530

    redis.sh

--- redis.sh
+++ redis.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Redis module enable"
 dnf install redis -y    &>>$LOG_FILE
 VALIDATE $? "Redis Installation"
 
-sed -i -e 's/127.0.0.0/0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.0/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Redis config file Edited & Updated"
 
 
