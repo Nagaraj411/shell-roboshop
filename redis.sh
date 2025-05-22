33a5cea84036e1e9599dd7c264f15ed374da00b4
Nagaraj <nagarajuande255@gmail.com>
2025-05-22 11:06:08 +0530

    redis.sh

--- redis.sh
+++ redis.sh
@@ -51,7 +51,6 @@ VALIDATE $? "Enabling Redis"
 systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "Started Redis       
 
-
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
