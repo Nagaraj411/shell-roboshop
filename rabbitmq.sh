5db25bd21163d0f565e5edae2b57ee0b90cfae63
Nagaraj <nagarajuande255@gmail.com>
2025-05-22 11:50:47 +0530

    rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -49,3 +49,11 @@ VALIDATE $? "RabbitMQ service enable"
 systemctl start rabbitmq-server   &>>$LOG_FILE
 VALIDATE $? "RabbitMQ service start"
 
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
