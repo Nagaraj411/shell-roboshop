37cb5f8bf55633e68581597544ae2a74df4b559e
Nagaraj <nagarajuande255@gmail.com>
2025-05-23 09:55:51 +0530

    rabbitmq.sh

--- mysql.sh
+++ mysql.sh
@@ -53,3 +53,15 @@ END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
+
+systemctl
+    if [ $? -eq 0 ]
+    then
+        echo "MySQL is Successfully running"
+    else
+        echo "MySQL Failed to start"
+        tail -f /var/log/mysqld.log # Check the MySQL log for errors
+        echo "Please check the log file for more details"
+        exit 1
+    fi
